fb5c80909ef496aaf1075f91035bc811de08000a
geistmeister <74523173+blightchild@users.noreply.github.com>
2023-05-23 00:04:29 -0500

    Add files via upload

---
+++ getGrade.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void gradeFunc(int element);
+
+void gradeFunc(int element){
+	  if(element >= 90){
+                std::cout << "A" << std::endl;
+        }
+        else if (element == 90 || element >= 80){
+                std::cout << "B" << std::endl;
+        }
+        else if(element <= 80 || element >= 70){
+                std::cout << "C" << std::endl;
+        }
+        else if(element <= 70 || element >= 60){
+                std::cout << "D" << std::endl;
+        }
+        else if(element <= 60 || element <= 50){
+                std::cout << "F" << std::endl;
+        }
+        else{
+                std::cout << "Invalid Grade";
+        }
+}
+
+int main()
+{
+	int grade;
+
+	std::cout << "Enter your grade" << std::endl;
+	std::cin >> grade;
+
+	gradeFunc(grade);
+
+	return 0;
+}
